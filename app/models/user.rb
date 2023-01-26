c4680c92c65e23b898173667ba0a8dac0137c949
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2023-01-26 17:29:15 +0530

    added bcrypt and user model

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# User
+class User < ApplicationRecord
+  has_secure_password
+
+  before_save :downcase_email
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+end
